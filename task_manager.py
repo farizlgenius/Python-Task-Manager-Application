ca55c6ddee74d62316d6225ba74bcce88ca109d0
farizlgenius <farizlengha040@gmail.com>
2025-12-26 22:00:49 +0700

    develop: adding view tasks and mark complete feature

--- task_manager.py
+++ task_manager.py
@@ -1,53 +1,70 @@
 import file_manager as fm
 import task as tk
+import datetime as dt
 
 class TaskManager:
     def __init__(self):
-        pass
-    
+        datas = fm.FileManager().read_file()
+        for data in datas:
+            if data['status'] == 'pending' and 'due_date' in data and dt.datetime.strptime(data['due_date'], "%Y-%m-%d").date() < dt.datetime.now().date():
+                data['status'] = 'overdue'
+        self.tasks = datas
+
     def create_task(self,title, description, due_date):
-        tasks = fm.FileManager().read_file()
         id = 0
-        for task in tasks:
+        for task in self.tasks:
           if id <= task['id']:
             id = task['id'] + 1
         data = {
             "id": id,
             "title": title,
             "description": description,
-            "status": "incomplete",
+            "status": "pending",
             "due_date": due_date
         }
         self.save_task(data)
 
     def show_all_tasks(self):
-        tasks = fm.FileManager().read_file()
-        completed_tasks = [task for task in tasks if task['status'].lower() == 'complete']
-        overdue_tasks = [task for task in tasks if task['status'].lower() == 'overdue']
-        pending_tasks = [task for task in tasks if task['status'].lower() == 'pending']
+        completed_tasks = [task for task in self.tasks if task['status'].lower() == 'complete']
+        overdue_tasks = [task for task in self.tasks if task['status'].lower() == 'overdue']
+        pending_tasks = [task for task in self.tasks if task['status'].lower() == 'pending']
         self.print_table(completed_tasks, "Here are all the tasks with COMPLETE status:")
         self.print_table(overdue_tasks, "Here are all the tasks with OVERDUE status:")
         self.print_table(pending_tasks, "Here are all the tasks with PENDING status:")
 
     def show_completed_tasks(self):
-        tasks = fm.FileManager().read_file()
-        completed_tasks = [task for task in tasks if task['status'].lower() == 'complete']
+        completed_tasks = [task for task in self.tasks if task['status'].lower() == 'complete']
         self.print_table(completed_tasks, "Here are all the tasks with COMPLETE status:")
 
     def show_overdue_tasks(self):
-        tasks = fm.FileManager().read_file()
-        overdue_tasks = [task for task in tasks if task['status'].lower() == 'overdue']
+        overdue_tasks = [task for task in self.tasks if task['status'].lower() == 'overdue']
         self.print_table(overdue_tasks, "Here are all the tasks with OVERDUE status:")
 
     def show_pending_tasks(self):
-        tasks = fm.FileManager().read_file()
-        pending_tasks = [task for task in tasks if task['status'].lower() == 'pending']
+        pending_tasks = [task for task in self.tasks if task['status'].lower() == 'pending']
         self.print_table(pending_tasks, "Here are all the tasks with PENDING status:")
+        return pending_tasks.__len__()
+
+    def complete_task(self):
+        num = self.show_pending_tasks()
+        if num == 0:
+            print("No pending tasks to complete.")
+            return
+        try:
+            input_id = int(input("Enter the Task ID to mark as complete: ").strip())
+        except ValueError:
+            print("Invalid input. Please enter a valid Task ID.")
+            return
+        for task in self.tasks:
+            if task['id'] == input_id:
+                task['status'] = 'complete'
+                fm.FileManager().write_file(self.tasks)
+                return
+        print(f"Task with ID {input_id} not found.")
 
     def save_task(self, task):  
-        tasks = fm.FileManager().read_file()
-        tasks.append(task)
-        fm.FileManager().write_file(tasks)
+        self.tasks.append(task)
+        fm.FileManager().write_file(self.tasks)
 
     def print_table(self,tasks, title):
       if not tasks:
