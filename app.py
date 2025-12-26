38f559aca8da5e9993c08128771292e4695b8040
farizlgenius <farizlengha040@gmail.com>
2025-12-26 20:50:13 +0700

    develop: create file manager class

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
+import file_manager as fm
+
+
 runnning_flag = True
+file_path = 'data.json'
+
 
 while runnning_flag:
     print('''
           ####################################
           Welcome to Task Manager Application!
           ####################################
+
           Please see menu details below:
           1. Add Task
-          2. View Tasks
-          3. Delete Task
-          4. Exit
+          2. View All Tasks
+          4. Complete Task
+          5. Delete Task
+          7. Search Task
+          6. Exit
           Please select an option from the menu (1,2,3,4).
+          
           ####################################
           ''')
     user_input = input("Enter the menu option: ").strip()
 
     if user_input == '4':
         runnning_flag = False
-        print("Exiting the program.")
+        print("Exiting the program...")
     elif user_input not in ['1', '2', '3']:
         print("Invalid option. Please try again.")
     elif user_input == '1':
         print("You selected: Add Task")
+        task_list = fm.FileManager(file_path).read_file()
+        for task in task_list:
+            print(f"Task ID: {task['id']}, Title: {task['title']}, Due Date: {task['due_date']}")
     elif user_input == '2':
         print("You selected: View Tasks")
     elif user_input == '3':
---
+++ file_manager.py
@@ -0,0 +1,15 @@
+import json
+
+class FileManager:
+  def __init__(self,file_path):
+    self.file_path = file_path
+
+  def read_file(self):
+    with open(self.file_path,'r') as file:
+      content = file.read()
+      print(content)
+    return json.loads(content)
+
+  def write_file(self, data):
+    with open(self.file_path, 'w') as file:
+      json.dump(data, file, indent=4)
